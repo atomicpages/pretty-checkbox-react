eaf8be44c0df275cfe5c078b1fa9202f5c1162cc
Dennis Thompson <djtthompson@gmail.com>
2019-01-20 19:04:23 -0800

    Updates

---
+++ precache-manifest.e52b992a82edcdba94e5e1271e23325a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fa05ba959c9362c6fa1",
+    "url": "/pretty-checkbox-react/static/js/runtime~main.5fa05ba9.js"
+  },
+  {
+    "revision": "b7fac1d7f4f4fc20d606",
+    "url": "/pretty-checkbox-react/static/js/main.b7fac1d7.chunk.js"
+  },
+  {
+    "revision": "eb2fa0ec2a7691a48d11",
+    "url": "/pretty-checkbox-react/static/js/1.eb2fa0ec.chunk.js"
+  },
+  {
+    "revision": "b7fac1d7f4f4fc20d606",
+    "url": "/pretty-checkbox-react/static/css/main.1f5885cd.chunk.css"
+  },
+  {
+    "revision": "c9c6917720fa3f69114e0505c92c031b",
+    "url": "/pretty-checkbox-react/index.html"
+  }
+];
