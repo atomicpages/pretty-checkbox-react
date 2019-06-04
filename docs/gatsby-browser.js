0e77cbbc5c92532b02db54fb81f14ed2a6c1699f
atomicpages <djtthompson@gmail.com>
2019-06-03 18:55:05 -0700

    Moved doc site

--- docs/gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-require('pretty-checkbox/dist/pretty-checkbox.min.css');
-require('prismjs/plugins/line-numbers/prism-line-numbers.css');
-require('prismjs/themes/prism-tomorrow.css');
